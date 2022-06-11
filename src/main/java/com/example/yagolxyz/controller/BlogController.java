e81a2a2ff41cb5bf7cd869cbd168d5cbd0ff649f
yagol <yhw_yagol@yeah.net>
2022-06-11 08:19:32 +0800

    docker support
    Signed-off-by: yagol <yhw_yagol@yeah.net>

--- src/main/java/com/example/yagolxyz/controller/BlogController.java
+++ src/main/java/com/example/yagolxyz/controller/BlogController.java
@@ -3,6 +3,7 @@ package com.example.yagolxyz.controller;
 import com.example.yagolxyz.entity.Category;
 import com.example.yagolxyz.service.BlogService;
 import com.example.yagolxyz.vo.BlogVo;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -14,12 +15,14 @@ import java.util.List;
  * @date 2022/5/24
  */
 @RestController
+@Slf4j
 public class BlogController {
     @Autowired
     private BlogService blogService;
 
     @GetMapping("/blogs")
     public List<BlogVo> get(Category category) {
+        log.info("get blogs");
         return blogService.listVoByCategory(category);
     }
 }
